e6e91d1c79f43f6fc11416f7a9562bde8088961a
関田健佑 <greeman.apon199711@gmail.com>
2020-10-03 11:00:09 +0900

    タイムラインに画像表示

--- lib/pages/time_line.dart
+++ lib/pages/time_line.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:insta_clone/contents.dart';
 import 'package:insta_clone/domain/post_model.dart';
+import 'package:insta_clone/widgets/molecules/post_item.dart';
 
 class TimeLine extends StatefulWidget {
   TimeLine({Key key, this.title}) : super(key: key);
@@ -14,12 +14,12 @@ class _TimeLineState extends State<TimeLine> {
   ScrollController _scrollController;
   bool _isLoading = false;
 
-  List<String> _items;
+  List<PostModel> _items;
 
   @override
   void initState() {
-    _items = List<String>();
-    Contents.forEach((content) => _items.add(content));
+    _items = List<PostModel>();
+    PostModel.dummyData.forEach((content) => _items.add(content));
 
     _scrollController = ScrollController();
     _scrollController.addListener(() {
@@ -43,7 +43,7 @@ class _TimeLineState extends State<TimeLine> {
       body: ListView.builder(
         controller: _scrollController,
         itemBuilder: (context, index) {
-          return _buildListItemSample(_items[index]);
+          return PostItem(_items[index]);
         },
         itemCount: _items.length,
       ),
@@ -71,7 +71,7 @@ class _TimeLineState extends State<TimeLine> {
     _isLoading = true;
     Future.delayed(Duration(seconds: 2), () {
       setState(() {
-        Contents.forEach((content) => _items.add(content));
+        PostModel.dummyData.forEach((content) => _items.add(content));
       });
       _isLoading = false;
     });
--- lib/widgets/molecules/post_item.dart
+++ lib/widgets/molecules/post_item.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone/domain/post_model.dart';
 
 /// タイムラインに表示する投稿Item
 class PostItem extends StatelessWidget {
+  PostItem(this.model);
+  final PostModel model;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(
+            radius: 24.0,
+            backgroundImage: NetworkImage(model.userIcon),
+          ),
+          title: Row(
+            children: <Widget>[
+              Text(model.userName),
+              SizedBox(
+                width: 16.0,
+              ),
+            ],
+          ),
+        ),
+        Center(
+          child: Image(// Imageウィジェット
+            image: NetworkImage(model.postImage),// 表示したい画像
+          ),
+        ),
+      ],
     );
   }
 }
