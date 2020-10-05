31d0c873eec47973915b4e364f8055bf478b450e
関田健佑 <greeman.apon199711@gmail.com>
2020-10-05 22:53:12 +0900

    みためを修正

--- lib/widgets/molecules/post_item.dart
+++ lib/widgets/molecules/post_item.dart
@@ -9,33 +9,45 @@ class PostItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin:EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            leading: CircleAvatar(
-              radius: 24.0,
-              backgroundImage: NetworkImage(model.userIcon),
-            ),
-            title: Row(
-              children: <Widget>[
-                Text(model.userName),
-                SizedBox(
-                  width: 16.0,
-                ),
-              ],
-            ),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            spreadRadius: 1.0,
+            blurRadius: 10.0,
+            offset: Offset(10, 10),
           ),
-          Center(
-            child: Image(// Imageウィジェット
-              image: NetworkImage(model.postImage),// 表示したい画像
-              height: 400,
-              fit:BoxFit.fill
-            ),
-          ),
-          Text(model.postText != null ? model.postText : '...'),
         ],
-      )
+      ),
+      margin:EdgeInsets.all(8.0),
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                radius: 24.0,
+                backgroundImage: NetworkImage(model.userIcon),
+              ),
+              title: Row(
+                children: <Widget>[
+                  Text(model.userName),
+                  SizedBox(
+                    width: 16.0,
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Image(// Imageウィジェット
+                image: NetworkImage(model.postImage),// 表示したい画像
+                height: 400,
+                fit:BoxFit.fill
+              ),
+            ),
+            Text(model.postText != null ? model.postText : '...'),
+          ],
+        )
+      ),
     );
   }
 }
